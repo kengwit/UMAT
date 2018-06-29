8fcda12cf136fd1dbd587db51a617aa85fa511cb
scratch <p2sathan@uwaterloo.ca>
2018-06-29 14:49:15 -0400

    removed previous assignment files

--- A2_725/A2.f
+++ A2_725/A2.f
@@ -6,19 +6,32 @@ c
          double precision :: UP1,UP2,UP3
          double precision :: eu1,eu2,eu3
          double precision :: la1,la2,la3
+         double precision :: gam, lam
+         gam = 2.0_8
+         lam = 0.5_8
 c
-         F = (/ 1.0_8, 2.0_8, 0.0_8,
-     &          0.0_8, 1.0_8, 0.0_8,
-     &          0.0_8, 0.0_8, 1.0_8 /)
+c    Simple Shear Def. Grad.
+c
+c         F = (/ 1.0_8, gam, 0.0_8,
+c     &          0.0_8, 1.0_8, 0.0_8,
+c     &          0.0_8, 0.0_8, 1.0_8 /)
+c
+c   Plain Strain Stretching Def. Grad.
+c
+c         F = (/ 1.0_8, gam, 0.0_8,
+c     &          0.0_8, lam, 0.0_8,
+c     &          0.0_8, 0.0_8, 1.0_8/lam /)
+c
+c   Equal Biaxial Stretching Def. Grad.
+c
+         F = (/ lam, gam, 0.0_8,
+     &          0.0_8, lam, 0.0_8,
+     &          0.0_8, 0.0_8, 1/(lam*lam) /)
 c
         call PD (F,R,U,V)
-c        call testPD (F)
 c
         call CH (V,Vp)
         call CH (U,Up)
-c        print *,
-c        call t2print (Vp)
-c        print *,
         VP1 = Vp(1)
         VP2 = Vp(5)
         VP3 = Vp(9)
@@ -26,15 +39,15 @@ c        print *,
         eu2 = LOG(VP2)
         eu3 = LOG(VP3)
 c
-        UP1 = Up(1)
-        UP2 = Up(5)
-        UP3 = Up(9)
-        la1 = LOG(UP1)
-        la2 = LOG(UP2)
-        la3 = LOG(UP3)
+c        UP1 = Up(1)
+c        UP2 = Up(5)
+c        UP3 = Up(9)
+c        la1 = LOG(UP1)
+c        la2 = LOG(UP2)
+c        la3 = LOG(UP3)
 c
         print "(3e12.4)",eu1,eu2,eu3
 c
-        print "(3e12.4)",la1,la2,la3
+c        print "(3e12.4)",la1,la2,la3
 c
         end program A2_725

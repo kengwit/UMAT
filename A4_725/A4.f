0b00d9eb47122b0e300aab98d2af9e24309b9b31
scratch <p2sathan@uwaterloo.ca>
2018-07-31 17:02:18 -0400

    Nightly compile. Secant method diverging. Fix this.

--- A4_725/A4.f
+++ A4_725/A4.f
@@ -7,9 +7,13 @@ c        THESE ARE OUTPUTS
 c
          double precision :: sig(9), ep (9), eps
          double precision :: dtrial (9), strial (9)
+         double precision :: d22trial (7), s22trial (7)
          double precision :: d33trial (7), s33trial (7)
          double precision :: deps (9), cm (5), hisv (11)
-         double precision :: xnm1, xnm2, fnm1, fnm2, deltas
+         double precision :: x1nm1, x1nm2, f1nm1, deltas22
+         double precision :: x2nm1, x2nm2, f2nm1, deltas33
+         double precision :: f1x1m2, f1x2m2, f2x1m2, f2x2m2
+         double precision :: df1dx1, df1dx2, df2dx1, df2dx2
          double precision :: tol
          integer :: i,final,cnt
 c
@@ -34,10 +38,12 @@ c
 c
          dtrial = 0.0_8
          strial = 0.0_8
+         d22trial = 0.0_8
+         s22trial = 0.0_8
          d33trial = 0.0_8
          s33trial = 0.0_8
          cnt = 1
-         final = 1000
+         final = 2
 c
          do i = 1,final 
 c
@@ -45,66 +51,96 @@ c
               deps (1) = 0.001_8
               eps = hisv(1)
 c
-c             initial guess for D33
+c   Step 1: Guess bounds of d22, d33
 c
-              deps (9) = 0.000_8
-c
-c  The following code is an implementation of the Secant
-c  method as documented on the wiki page for the Secant 
-c  method... It is pretty clunky as I am iterating off a
-c  umat call. Because of this, I have to make sure that I am
-c  not affecting the stress and strain state of the previous 
-c  timestep. All states should be saved before the iteration,
-c  reset at each iteration and ONLY UPDATED after the solution
-c  has converged (at least for plain strain)
-c
-c  Step 1: 
-c
-c  Make a guess at where the root is (upper and lower bound)
-c  I used +/- D11. Since D22 = 0, D33 should be the shinkage
-c  due to D11 so it seems reasonable to assume it is somewhere
-c  between (-D11,+D11)...
-c
-c
+              d22trial(1) = -deps(1)
+              d22trial(2) = deps(1)
               d33trial(1) = -deps(1)
               d33trial(2) = deps(1)
 c
-c Step 2: Find stress at guesses so that we can find the derivative
-c         for the secant method
+c   Step 2: Find cooresponding s22,s33
 c
               dtrial = deps
               strial = sig
               hisv(1) = eps
+              dtrial(5) = d22trial(1)
               dtrial(9) = d33trial(1)
-              call umat_43 (cm,strial,dtrial,hisv,0.0_8,0.0_8)
-              s33trial(1) = strial (9)
+              call umat_43(cm,strial,dtrial,hisv,0.0_8,0.0_8)
+              s22trial(1) = strial(5)
+              s33trial(1) = strial(9)             
+c
               dtrial = deps
               strial = sig
               hisv(1) = eps
+              dtrial(5) = d22trial(2)
               dtrial(9) = d33trial(2)
-              call umat_43 (cm,strial,dtrial,hisv,0.0_8,0.0_8)
-              s33trial(2) = strial (9)
-              deltas = 100
-              cnt = 3
+              call umat_43(cm,strial,dtrial,hisv,0.0_8,0.0_8)
+              s22trial(2) = strial(5)
+              s33trial(2) = strial(9)
+              deltas22 = 100
+              deltas33 = 100
+              cnt = 3             
+c             
+              do while (    (DABS(deltas22).GT.tol*100)
+     &                 .AND.(DABS(deltas33).GT.tol*100)
+     &                 .AND.(cnt.LT.200)   )
 c
-c Step 3: Iterate on D33 s.t. S33 = 0
+                  dtrial = deps
+                  strial = sig
+                  hisv(1) = eps
 c
-              do while ((DABS(deltas).GT.tol*100).AND.(cnt.LT.7))
-                    hisv(1) = eps
-                    dtrial = deps
-                    strial = sig
-                    xnm1 = d33trial (cnt-1)
-                    xnm2 = d33trial (cnt-2)
-                    fnm1 = s33trial (cnt-1)
-                    fnm2 = s33trial (cnt-2)
-                    d33trial(cnt) = 
-     &                      xnm1 - fnm1 * ((xnm1-xnm2)/(fnm1-fnm2))
-                    dtrial(9) = d33trial (cnt)
-                    call umat_43(cm,strial,dtrial,hisv,0.0_8,0.0_8)
-                    s33trial(cnt) = strial (9)
-c                    print "(9e15.6)",s33trial(cnt)
-                    deltas = s33trial (cnt) - s33trial(cnt-1)
-                    cnt = cnt + 1
+                  x1nm1 = d22trial(cnt-1)
+                  x1nm2 = d22trial(cnt-2)
+                  x2nm1 = d33trial(cnt-1)
+                  x2nm2 = d33trial(cnt-2)
+c
+                  f1nm1 = s22trial(cnt-1)
+                  f2nm1 = s33trial(cnt-1)
+c
+                  dtrial = deps
+                  strial = sig
+                  hisv(1) = eps
+                  dtrial(5) = d22trial(cnt-2)
+                  dtrial(9) = d33trial(cnt-1)
+                  call umat_43(cm,strial,dtrial,hisv,0.0_8,0.0_8)
+                  f1x1m2 = strial(5)
+                  f2x1m2 = strial(9)             
+c
+                  dtrial = deps
+                  strial = sig
+                  hisv(1) = eps
+                  dtrial(5) = d22trial(cnt-1)
+                  dtrial(9) = d33trial(cnt-2)
+                  call umat_43(cm,strial,dtrial,hisv,0.0_8,0.0_8)
+                  f1x2m2 = strial(5)
+                  f2x2m2 = strial(9)             
+c
+                  df1dx1 = (f1nm1-f1x1m2)/(x1nm1-x1nm2)
+                  df1dx2 = (f1nm1-f1x2m2)/(x2nm1-x2nm2)
+                  df2dx1 = (f2nm1-f2x1m2)/(x1nm1-x1nm2)
+                  df2dx2 = (f2nm1-f2x2m2)/(x2nm1-x2nm2)
+c
+                  d22trial(cnt) = x1nm1
+     &                          - f1nm1 / df1dx1 - f2nm1 / df2dx1
+c
+                  d33trial(cnt) = x2nm1 
+     &                          - f1nm1 / df1dx2 - f2nm1 / df2dx2
+
+c
+                  hisv(1) = eps
+                  dtrial = deps
+                  strial = sig
+                  dtrial(5) = d22trial(cnt)
+                  dtrial(9) = d33trial(cnt)
+                  call umat_43(cm,strial,dtrial,hisv,0.0_8,0.0_8)
+                  s22trial(cnt) = strial(5)
+                  s33trial(cnt) = strial(9)
+                  deltas22 = s22trial(cnt) - s22trial(cnt-1)
+                  deltas33 = s33trial(cnt) - s33trial(cnt-1)
+                  print "(2e15.6)",d22trial(cnt-1),d22trial(cnt)
+c                  print "(4e15.6)",f1nm1,df1dx2,f2nm1,df2dx2
+                  cnt = cnt + 1                 
+c
               end do
 c
 c Step 4: Update stress and strain accordingly
@@ -118,7 +154,8 @@ c  Regardless, I do not have F so I can't find W
 c
               ep = ep + deps
 c
-              print "(20e15.6)",ep,sig,hisv(1),hisv(11)
+               print "(9e15.6)",sig
+c              print "(20e15.6)",ep,sig,hisv(1),hisv(11)
 c
          end do
 c
